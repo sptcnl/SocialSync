ae9f0c9b4bc32e3ba44472a7e192de1982e1bd2f
sptcnl <sptcnl@gmail.com>
2024-11-10 22:01:05 +0900

    fix: 같은 경로인 모듈 모으기

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from django.contrib.auth import get_user_model, authenticate
 from .serializers import SignupSerializer
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.views import APIView
 
 from rest_framework_simplejwt.tokens import RefreshToken
