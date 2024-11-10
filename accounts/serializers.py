b10d524159a5ade65686854051078764481c53b3
sptcnl <sptcnl@gmail.com>
2024-11-10 20:49:12 +0900

    update: 회원가입 시 Response Message에 roles 표시

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,6 +7,11 @@ from accounts.models import Role, UserRole
 
 User = get_user_model()
 
+class RoleSerializer(serializers.Serializer):
+    class Meta:
+        model = Role
+        fields = ('role')
+
 class SignupSerializer(serializers.Serializer):
     username = serializers.CharField(
         required=True, 
@@ -18,10 +23,20 @@ class SignupSerializer(serializers.Serializer):
         validators=[validate_password]
         )
     nickname = serializers.CharField(required=True)
+    roles = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'nickname')
+        fields = ('username', 'password', 'nickname', 'roles')
+
+    def get_roles(self, obj):
+        # 해당 유저가 가진 역할 목록을 반환 (UserRole 중계 테이블을 통해)
+        user_roles = UserRole.objects.filter(user=obj)
+        
+        # 역할이 있을 경우 직렬화된 데이터 반환, 없을 경우 빈 리스트 반환
+        if user_roles.exists():
+            return RoleSerializer([user_role.role for user_role in user_roles], many=True).data
+        return []  # 역할이 없으면 빈 리스트 반환
 
     def create(self, validated_data):
         # 사용자 생성
