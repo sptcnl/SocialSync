2d77f2d537162c3b1734e15b812ccef275cfff40
sptcnl <sptcnl@gmail.com>
2024-11-10 23:07:41 +0900

    update: ALLOWED_HOSTS 추가

--- socialsync/settings.py
+++ socialsync/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "43.201.18.203",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
